6db750bac4e57bfe7f9d527c7e619ddb60b47e4c
Taanveer22 <taanveer469@gmail.com>
2025-07-10 01:40:23 +0600

    alhamdulliah fetch done and button showed in the ui

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 console.log("conneted js");
 
 // load data from api
-function loadCategories() {
+const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data) => displayCategories(data.categories));
-}
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
 
 // show data in the ui
-function displayCategories(paramCategory) {
-  console.log(paramCategory);
-}
+// create dynamic button
+const displayCategories = (paramCategory) => {
+  //   console.log(paramCategory);
+  const categoryContainer = document.getElementById("category-container");
+  //   console.log(categoryContainer);
+  paramCategory.forEach((item) => {
+    console.log(item);
+
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+    categoryContainer.append(button);
+  });
+};
 
 // function invocation
 loadCategories();
