5e3e8243479068e8e0ab28f4c48d85847f5a44fb
Taanveer22 <taanveer469@gmail.com>
2025-07-10 01:16:14 +0600

    alhamdulliah data fetched from api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('conneted js');
+console.log("conneted js");
+
+// load data from api
+function loadCategories() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
+}
+
+// show data in the ui
+function displayCategories(paramCategory) {
+  console.log(paramCategory);
+}
+
+// function invocation
+loadCategories();
